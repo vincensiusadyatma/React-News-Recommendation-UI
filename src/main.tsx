d8d30e4aa8557cbba35b1656e3ee20333be8fe4d
vincenciusadyatma <adyatmav@gmail.com>
2026-01-28 12:05:53 +0700

    feat: create login ui pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter,Routes, Route } from 'react-router'
 import './index.css'
 import App from './App.tsx'
+import LoginPage from './pages/LoginPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/auth/login' element={<LoginPage/>}/>
+
+
+      </Routes>
+      
+ 
+    </BrowserRouter>
+    
   </StrictMode>,
 )

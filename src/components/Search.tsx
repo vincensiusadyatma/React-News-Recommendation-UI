7a08b3b95fe46a916660bf8ff35dc608b0d3167e
vincenciusadyatma <adyatmav@gmail.com>
2026-03-14 17:58:12 +0700

    fix: change search bar color

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -22,12 +22,15 @@ const Search = ({ inputQuery, setInputQuery, onSearch }: SearchBarProps) => {
         placeholder="Search news..."
         value={inputQuery}
         onChange={(e) => setInputQuery(e.target.value)}
-        className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="flex-1 px-4 py-2 border border-white/30 rounded-lg 
+        bg-transparent text-white placeholder-white/70
+        focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
 
       <button
         type="submit"
-        className="px-5 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+        className="cursor-pointer px-5 py-2 rounded-lg text-white 
+        bg-blue-600 hover:bg-blue-700 transition font-medium"
       >
         Search
       </button>
